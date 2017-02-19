082816be8f7507bedcd68c63d4d4a24fa1a33b01
Christopher N. Hesse <raymanfx@gmail.com>
2017-02-19 20:33:50 +0100

    nobleltetmo: Set correct bcmdhd firmware paths
    Change-Id: If908d2ffe077a0835c6480a30f872931716beb34

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,10 @@ TARGET_KERNEL_CONFIG := cm_nobleltetmo_defconfig
 # RIL
 BOARD_MODEM_TYPE := ss333
 
+# WiFi bcmdhd firmware
+WIFI_DRIVER_FW_PATH_STA := "/etc/wifi/bcmdhd_sta.bin_b1"
+WIFI_DRIVER_FW_PATH_AP  := "/etc/wifi/bcmdhd_apsta.bin_b1"
+
 # Recovery
 TARGET_OTA_ASSERT_DEVICE := nobleltetmo,noblelte
 
