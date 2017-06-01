ce17040131bc3761ec97218a24550e1f24f9cdfb
Christopher N. Hesse <raymanfx@gmail.com>
2017-06-01 18:30:10 +0200

    nobleltetmo: Assert bootloader version
    Change-Id: I523d780c873fc71d11073ab800e81fe9e174eb9a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,6 +3,9 @@ LOCAL_PATH := device/samsung/nobleltetmo
 # Include path
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
+# Assertions
+TARGET_BOARD_INFO_FILE := $(LOCAL_PATH)/board-info.txt
+
 # Kernel
 TARGET_KERNEL_CONFIG := cm_nobleltetmo_defconfig
 
